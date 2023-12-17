a60891f0eb46688d987b69947df82b8cb44ec58f
EzBl4ck <federico.capoccia@proton.me>
2023-12-18 00:31:30 +0100

    .

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,9 +1,9 @@
 vim.api.nvim_create_user_command('CMakeDebug', function()
   require("toggleterm").exec(
-    "rm compile_commands.json && cmake -G Ninja -S . -B Debug -D CMAKE_BUILD_TYPE=DEBUG && ninja -C Debug");
+    "cmake -G Ninja -S . -B Debug -D CMAKE_BUILD_TYPE=DEBUG && ninja -C Debug && ln -sf Debug/compile_commands.json compile_commands.json");
 end, {})
 
 vim.api.nvim_create_user_command('CMakeRelease', function()
   require("toggleterm").exec(
-    "rm compile_commands.json && cmake -G Ninja -S . -B Release -D CMAKE_BUILD_TYPE=Release && ninja -C Release");
+    "cmake -G Ninja -S . -B Release -D CMAKE_BUILD_TYPE=Release && ninja -C Release && ln -sf Release/compile_commands.json compile_commands.json");
 end, {})
