eeba6aff549fb17f1b14328446dd8ca1576f831a
EzBl4ck <federico.capoccia@proton.me>
2024-01-01 19:22:53 +0100

    .

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,50 +1,50 @@
 -----------------------------------------
----   ___        _   _                 
----  / _ \ _ __ | |_(_) ___  _ __  ___ 
+---   ___        _   _
+---  / _ \ _ __ | |_(_) ___  _ __  ___
 --- | | | | '_ \| __| |/ _ \| '_ \/ __|
 --- | |_| | |_) | |_| | (_) | | | \__ \
 ---  \___/| .__/ \__|_|\___/|_| |_|___/
----       |_|                          
+---       |_|
 --- By EzBl4ck
 -----------------------------------------
 
 local options = {
-	backup = false,
-	clipboard = "unnamedplus",
-	cmdheight = 1,
-	conceallevel = 0,
-	hlsearch = true,
-	ignorecase = false,
-	mouse = "a",
-	showmode = true,
-	showtabline = 2,
-	number = true,
-	relativenumber = true,
-	smartcase = true,
-	smartindent = true,
-	splitbelow = true,
-	splitright = true,
-	swapfile = false,
-	undofile = true,
-	updatetime = 300,
-	expandtab = true,
-	shiftwidth = 2,
-	tabstop = 2,
-	wrap = false,
-	scrolloff = 8,
-	sidescrolloff = 8,
-	termguicolors = true,
+  backup = false,
+  clipboard = "unnamedplus",
+  cmdheight = 1,
+  conceallevel = 0,
+  hlsearch = true,
+  ignorecase = false,
+  mouse = "a",
+  showmode = true,
+  showtabline = 2,
+  number = true,
+  relativenumber = true,
+  smartcase = true,
+  smartindent = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  undofile = true,
+  updatetime = 300,
+  expandtab = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  wrap = false,
+  scrolloff = 8,
+  sidescrolloff = 8,
+  termguicolors = true,
   laststatus = 3,
 }
 
 for option, value in pairs(options) do
-	vim.opt[option] = value
+  vim.opt[option] = value
 end
 
 -- Providers --
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_perl_provider = 0
+--vim.g.loaded_python3_provider = 0
+--vim.g.loaded_ruby_provider = 0
+--vim.g.loaded_perl_provider = 0
 
 vim.opt.shortmess:append("c")
 vim.cmd([[set iskeyword+=-]])
