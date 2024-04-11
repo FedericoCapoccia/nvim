51e26fb5939568357c6928db082483e4efeacb36
Federico Capoccia <federico@git.com>
2024-04-12 00:10:11 +0200

    full lsp

--- lua/fede/lazy/lsp.lua
+++ lua/fede/lazy/lsp.lua
@@ -1,3 +1,184 @@
 return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "jay-babu/mason-null-ls.nvim",
+        "nvimtools/none-ls.nvim",
+        "stevearc/conform.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+        "simrat39/rust-tools.nvim",
+        "saecki/crates.nvim",
+    },
 
+    config = function()
+        local cmp = require("cmp")
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
+        require("fidget").setup({})
+        require("mason").setup()
+        require("mason-null-ls").setup({
+            ensure_installed = {
+                "prettierd",
+                "clang-format",
+            },
+            automatic_installation = true,
+            automatic_setup = true,
+            handlers = {}
+        })
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "gopls",
+                "clangd",
+            },
+            automatic_installation = true,
+            handlers = {
+                function(server_name)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+
+                ["clangd"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.clangd.setup {
+                        capabilities = capabilities,
+                        cmd = {
+                            "clangd",
+                            "--background-index",
+                            "--clang-tidy",
+                        }
+                    }
+                end,
+
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                --runtime = { version = "Lua 5.1" },
+                                diagnostics = {
+                                    globals = {
+                                        "vim",
+                                        "it",
+                                        "describe",
+                                        "before_each",
+                                        "after_each"
+                                    },
+                                }
+                            }
+                        }
+                    }
+                end,
+            }
+        })
+
+        cmp.setup({
+            mapping = cmp.mapping.preset.insert({
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                ["<Tab>"] = cmp.mapping.select_next_item(),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-d>'] = cmp.mapping.scroll_docs(4),
+            }),
+
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+
+            formatting = {
+                format = function(_, vim_item)
+                    vim_item.menu = nil
+                    return vim_item
+                end
+            },
+
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "buffer" },
+                { name = "path" },
+                { name = "crates" },
+            }),
+
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+        })
+
+        -- Rust-tools
+        local rust_tools = require('rust-tools')
+        rust_tools.setup({
+            server = {
+                on_attach = function(_, bufnr)
+                    vim.keymap.set('n', '<leader>ca', rust_tools.hover_actions.hover_actions, { buffer = bufnr })
+                end
+            }
+        })
+
+        require("crates").setup({
+            src = {
+                cmp = {
+                    enabled = true,
+                },
+            },
+        })
+
+        -- Formatting
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "lua_ls" },
+                javascript = { "prettierd" },
+                typescript = { { "prettierd", "prettier" } },
+                json = { { "prettierd", "prettier" } },
+                rust = { "rustfmt" },
+                cpp = { "clang-format" },
+                c = { "clang-format" },
+            },
+        })
+
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = { buffer = event.buf }
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                vim.keymap.set('n', '<C-Space>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+            end
+        })
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function(args)
+                require("conform").format({ bufnr = args.buf })
+            end,
+        })
+    end
 }
