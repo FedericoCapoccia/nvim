d8cfe9d42e77e2fcb117cec4087242f031e09f7f
Federico Capoccia <federico@git.com>
2024-04-12 00:58:50 +0200

    .

--- lua/fede/lazy/colors.lua
+++ lua/fede/lazy/colors.lua
@@ -1,9 +1,9 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 local colorschemes = {
@@ -12,6 +12,7 @@ local colorschemes = {
         name = "rose-pine",
         config = function()
             require("rose-pine").setup({
+                disable_background = true,
                 styles = {
                     bold = true,
                     italic = true,
