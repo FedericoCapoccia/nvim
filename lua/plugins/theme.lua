c0c395285951584989f9a27e92d953c62ea751f2
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-03-17 00:01:39 +0100

    rose-pine with vim.pack

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,22 @@
+vim.pack.add({{
+    src = 'https://github.com/rose-pine/neovim',
+    name = 'rose-pine',
+}})
+
+require("rose-pine").setup({
+    enable = {
+        legacy_highlights = false,
+    },
+
+    styles = {
+        transparency = true,
+        italic = false,
+        bold = true,
+    },
+
+    highlight_groups = {
+        Comment = { italic = true },
+    },
+})
+
+vim.cmd("colorscheme rose-pine")
