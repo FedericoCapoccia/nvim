da37c8ff32642fbae43fc6f16d5d6d0615703097
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-03-17 17:03:05 +0100

    fixed lsp on_attach callback

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -12,23 +12,62 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local telescope_builtin = require "telescope.builtin"
 
 vim.lsp.config("*", {
-    capabilities = capabilities,
-    on_attach = function(client, bufnr)
-        local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
+    capabilities = {
+        textDocument = {
+            semanticTokens = {
+                multilineTokenSupport = true,
+            },
+        },
+    },
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if not client then
+            return
         end
 
-        map("<leader>c", vim.lsp.buf.code_action, "Code action")
-        map("<F2>", vim.lsp.buf.rename, "Rename")
-        map("<leader>sd", vim.lsp.buf.hover, "Hover documentation")
-
-        map("gd", vim.lsp.buf.definition, "Go to definition")
-        map("gD", vim.lsp.buf.declaration, "Go to declaration")
-        map("gi", vim.lsp.buf.implementation, "Go to implementation")
-        map("gr", telescope_builtin.lsp_references, "Go to references")
+        local function opts(desc)
+            return { buffer = ev.buf, desc = "LSP " .. desc }
+        end
 
         if client:supports_method "textDocument/inlayHint" then
-            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+            vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
+        end
+
+        if client:supports_method "textDocument/completion" then
+            client.server_capabilities = vim.tbl_deep_extend("force", client.server_capabilities, capabilities)
+        end
+
+        if client:supports_method "textDocument/codeAction" then
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Code action")
+        else
+            print(client.name .. " DOES NOT support Code Actions!")
+        end
+
+        if client:supports_method "textDocument/definition" then
+            vim.keymap.set("n", "gd", function()
+                telescope_builtin.builtin.lsp_definitions()
+            end, opts "Go to definition")
+        end
+
+        if client:supports_method "textDocument/declaration" then
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
+        end
+
+        if client:supports_method "textDocument/implementation" then
+            vim.keymap.set("n", "gi", function()
+                telescope_builtin.lsp_implementations()
+            end, opts "Go to implementation")
+        end
+
+        if client:supports_method "textDocument/hover" then
+            vim.keymap.set("n", "<leader>sd", vim.lsp.buf.hover, opts "Hover documentation")
+        end
+
+        if client:supports_method "textDocument/rename" then
+            vim.keymap.set("n", "<f2>", vim.lsp.buf.rename, opts "Rename")
         end
     end,
 })
