bed866860279e007c57fe51e458eaf85197f1512
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-03-26 11:13:53 +0100

    Fixed Bug

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,6 @@ require "plugins.gitsigns"
 require "plugins.oil"
 require "plugins.mason"
 require "plugins.conform"
-require "plugins.lint"
 require "plugins.treesitter"
 require "plugins.telescope"
 
--- lua/plugins/lint.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add { "https://github.com/mfussenegger/nvim-lint" }
-
-require("lint").linters_by_ft = {
-    javascript = { "eslint" },
-    typescript = { "eslint" },
-    javascriptreact = { "eslint" },
-    typescriptreact = { "eslint" },
-
-}
