929fe9f8cdd92738d291f2742adf2ae2416f0515
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-03-30 00:14:54 +0200

    One day I'll actually start looking at what Copilot generates instead of just immediately pushing it.

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -91,4 +91,12 @@ require("nvim-autopairs").setup {
 }
 
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+cmp.event:on(
+    "confirm_done",
+    cmp_autopairs.on_confirm_done {
+        filetypes = {
+            typescriptreact = false,
+            javascriptreact = false,
+        },
+    }
+)
