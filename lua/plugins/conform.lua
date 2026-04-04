c3b01e1689669f798ad0ae613b8db5971f18dae1
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-04-04 15:51:04 +0200

    accidental commit

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,14 +2,14 @@ vim.pack.add { { src = "https://github.com/stevearc/conform.nvim", name = "confo
 
 require("conform").setup {
     formatters_by_ft = {
-        javascript = { "oxfmt" },
-        typescript = { "oxfmt" },
-        javascriptreact = { "oxfmt" },
-        typescriptreact = { "oxfmt" },
-        json = { "oxfmt" },
-        jsonc = { "oxfmt" },
-        html = { "oxfmt" },
-        css = { "oxfmt" },
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        json = { "prettierd" },
+        jsonc = { "prettierd" },
+        html = { "prettierd" },
+        css = { "prettierd" },
         lua = { "stylua" },
     },
 
