429a0214a71c3acf58a88906e269a0f757e56a99
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-04-11 13:42:39 +0200

    I don't really feel like writing a commit

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -35,8 +35,6 @@ vim.lsp.config("zls", {
     settings = {
         zls = {
             enable_argument_placeholders = false,
-            enable_build_on_save = true,
-            build_on_save_step = "check",
         },
     },
 })
