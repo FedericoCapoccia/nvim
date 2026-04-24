2e731e4360db701de8648faa6f5b61874bfffa11
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-04-24 23:49:04 +0200

    aaa

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -75,7 +75,7 @@ cmp.setup {
                 nvim_lsp = "λ",
                 vsnip = "⋗",
                 buffer = "Ω",
-                path = "🖫",
+                path = "",
             }
             item.menu = menu_icon[entry.source.name]
 
