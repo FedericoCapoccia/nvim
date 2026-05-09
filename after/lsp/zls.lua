53eb8e04f7c092cb3bf90d4377ae6d08c30f978c
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-05-09 14:57:04 +0200

    I don't really feel like writing a commit

--- after/lsp/zls.lua
+++ after/lsp/zls.lua
@@ -2,6 +2,10 @@ return {
     settings = {
         zls = {
             enable_argument_placeholders = false,
+            inlay_hints_show_builtin = false,
+            inlay_hints_show_parameter_name = false,
+            inlay_hints_show_struct_literal_field_type = false,
+            inlay_hints_show_variable_type_hints = false,
         },
     },
 }
