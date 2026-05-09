a5a75cc77b2e816d052e7f94691059a33e5bf605
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-05-09 20:01:22 +0200

    I don't really feel like writing a commit

--- after/lsp/zls.lua
+++ after/lsp/zls.lua
@@ -1,6 +1,7 @@
 return {
     settings = {
         zls = {
+            build_on_save_args = { "-fincremental" },
             enable_argument_placeholders = false,
             inlay_hints_show_builtin = false,
             inlay_hints_show_parameter_name = false,
