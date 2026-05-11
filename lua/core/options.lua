b4597c809168378be26e4844374fd8b8a0c89cee
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-05-11 23:53:59 +0200

    I don't really feel like writing a commit

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -10,6 +10,7 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
+vim.opt.autoindent = true
 vim.opt.smartindent = true
 
 vim.g.markdown_recommended_style = 0
