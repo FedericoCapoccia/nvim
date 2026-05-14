5a731841671e10d7a4b430d70d2e7b63e722da78
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-05-14 15:38:15 +0200

    I don't really feel like writing a commit

--- after/lsp/rust_analyzer.lua
+++ after/lsp/rust_analyzer.lua
@@ -10,6 +10,21 @@ return {
                     snippets = "none", -- disables function argument placeholders
                 },
             },
+            inlayHints = {
+                bindingModeHints = { enable = false },
+                chainingHints = { enable = false },
+                closingBraceHints = { enable = false },
+                closureCaptureHints = { enable = false },
+                closureReturnTypeHints = { enable = "never" },
+                discriminantHints = { enable = "never" },
+                expressionAdjustmentHints = { enable = "never" },
+                implicitDrops = { enable = false },
+                lifetimeElisionHints = { enable = "never" },
+                parameterHints = { enable = false },
+                rangeExclusiveHints = { enable = false },
+                reborrowHints = { enable = "never" },
+                typeHints = { enable = false },
+            },
         },
     },
 }
